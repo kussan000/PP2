2706058bfb5d894844412ba5f9778eccfceabaf5
kussan000 <m_kussan@kbtu.kz>
2025-01-20 22:47:19 +0500

    last commit for today

---
+++ Lab1/W3/Strings.py
@@ -0,0 +1,22 @@
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
+
+b = "Hello, World!"
+print(b[-5:-2])
+
+a = "Hello, World!"
+print(a.split(",")) # returns ['Hello', ' World!']
+
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+
+txt = f"The price is {20 * 59} dollars"
+print(txt)
+
+txt = "We are the so-called \"Vikings\" from the north."
+
